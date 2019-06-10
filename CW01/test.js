0dd1b11f02b973b068e88d6d588077e56b7477fa
eihohil <igorgiley@gmail.com>
2019-06-10 16:58:49 +0300

    HM01 DONE

---
+++ CW01/test.js
@@ -0,0 +1,33 @@
+document.write('**********************************');
+document.write("<br>");
+
+alert('Введіть особисті дані');
+
+ y = prompt('Ваш вік');
+
+console.log(y, ':вік');
+
+
+document.write('1. ' + y + ' років');
+document.write("<br>");
+
+user1 = y
+
+alert('Введіть особисті дані');
+
+ y = prompt('Ваш вік');
+
+console.log(y, ':вік');
+
+
+document.write('2. ' + y + ' років');
+document.write("<br>");
+
+
+user2 = y
+
+user1 = (+user1 + +user2)/2;
+
+document.write('середнє знач');
+console.log (user1);
+document.write(user1);

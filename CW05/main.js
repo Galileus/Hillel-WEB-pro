b4270d51443b5c481375a3865b7cc8cb08cfbc97
Ihor <galileyforever@gmail.com>
2019-07-03 01:05:19 +0300

    cw05 changes

--- CW05/main.js
+++ CW05/main.js
@@ -23,6 +23,8 @@ a = 5;
 b = 5; 
 
 mass = new Array (a);
+summass = new Array ();
+temp = new Array ();
 
 for (i = 0; i < mass.length; i++){
  mass[i] = new Array(b);
@@ -35,21 +37,52 @@ for (i = 0; i < mass.length; i++){
 }
 console.log(mass, 'mass');
 
+for (i = 0, sum = 0; i < mass.length; i++){
+ 
 
 
-for (i = 0; i < mass.length; i++){
- for (j = 0, sum = 0; j < mass[i].length; j++){
-  sum = sum + mass[i][j];
-
-
-  
-
+  for (j = 0; j < mass[i].length; j++){
+  sum = sum + mass[i][j];  
  }
+ 
  console.log(sum, 'sum');
 
-/* maxsum = mass[i][0]
- minsum = mass[i][0]
-console.log(maxsum, 'maxsum');
-console.log(minsum, 'minsum');*/
- 
+summass[summass.length] = sum;
 }
+
+console.log(summass, 'summass');
+
+summax = summass[0];
+summin = summass[0];
+
+summaxi = 0
+summini = 0
+
+for (k = 1; k < summass.length; k++){
+  if (summass[k] > summax ){
+    summax = summass[k];
+    summaxi = k;
+  }
+
+  if (summass[k] < summin ){
+    summin = summass[k];
+    summini = k;
+  }
+}
+console.log(summin, 'summin');
+console.log(summini, 'summin index');
+
+
+console.log(summax, 'summax');
+console.log(summaxi, 'summin index');
+
+/*console.log(mass[summini], 'mass summini');*/
+
+temp = mass[summini];
+
+mass[summaxi] = temp;
+
+console.log(temp, 'temp');
+console.log(mass, 'mass');
+
+

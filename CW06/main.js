a2c6f090bbb3a81f20008100c29ebae3f9c4ccec
Ihor <galileyforever@gmail.com>
2019-07-07 17:35:19 +0300

    cw06

---
+++ CW06/main.js
@@ -0,0 +1,11 @@
+/*alert('hello');*/
+n = Math.ceil(Math.random() * (200 - 0) + 0);
+
+mass = new Array(n);
+
+
+for(i = 0; i < mass.length; i++){
+  mass[i] = Math.ceil(Math.random() * (200 - 0) + 0);
+
+}
+console.log(mass, 'mass');

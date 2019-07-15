a8e8284f548d9e7eb40cf38f759f51b8f742b793
eihohil <igorgiley@gmail.com>
2019-07-15 20:32:51 +0300

    cw11

--- CW11/main.js
+++ CW11/main.js
@@ -1,2 +1,13 @@
 //--------------1--------------------
 
+git init - створити локальний репозит
+
+cd ../ - крок назад
+
+cd./new folder - крок впередcd./new folder - крок впередcd./new folder - крок вперед
+
+
+$ git add ./
+
+$ git commit -m 'initial project version'
+

388c269b0d77f62f0fce74feb4ded665b7382d45
Ihor <galileyforever@gmail.com>
2019-07-18 23:26:26 +0300

    CW08 in progress

---
+++ CW08/main.js
@@ -0,0 +1,13 @@
+//--------------1--------------------
+
+function inArray (text, arr, func){
+
+for (var i = 0; i < arr.length ; i++) {
+    if (arr[i].indexOf(text) != -1){    
+    }
+  }
+return true;
+}
+console.log(inArray ('hello', ['ghdg', 'hello', 'dfgndfg']));
+
+

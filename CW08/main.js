4b3f825becd7d41ec31bcd6bd9c782a6120fc982
Ihor <galileyforever@gmail.com>
2019-07-20 13:24:24 +0300

    CW08 test

--- CW08/main.js
+++ CW08/main.js
@@ -1,6 +1,6 @@
 //--------------1--------------------
 
-function inArray (text, arr, func){
+/*function inArray (text, arr, func){
 
 for (var i = 0; i < arr.length ; i++) {
     if (arr[i].indexOf(text) != -1){    
@@ -9,5 +9,41 @@ for (var i = 0; i < arr.length ; i++) {
 return true;
 }
 console.log(inArray ('hello', ['ghdg', 'hello', 'dfgndfg']));
+*/
 
 
+
+/*var hotel = {
+
+  'name' : 'Home',
+  'status' : 'operational', 
+  'stars' : 3, 
+  'adv' : function (){
+    console.log(this.name + ' - The best hotel,' + this.stars + ' stars');
+  },
+
+  'hi' : 'hello',
+  'how much' : 500 
+
+};
+
+console.log(hotel.hi);
+hotel.adv();
+
+var a = {};
+
+a.hi = 'test';
+a.text = 'new text';
+a.func = function(){
+  console.log('hello jerk')
+}
+
+console.log(a);
+console.log(a.hi);
+a.func();
+console.log(a['hi']);
+console.log(hotel);
+
+var b = 'how much';
+
+console.log(hotel[b]);*/

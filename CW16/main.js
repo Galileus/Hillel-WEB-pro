a4f401440e7bc169c6f8633c69e364f20b1dcbb0
eihohil <igorgiley@gmail.com>
2019-08-02 18:37:31 +0300

    cw16 new files

---
+++ CW16/main.js
@@ -0,0 +1,21 @@
+
+
+window.onload = function(){
+    var box = document.querySelector('.box');
+
+    box.addEventListener('click', function(event) {
+        box.classList.toggle('active');
+        //event.target.box.classList.toggle('active');
+    }); 
+
+    document.addEventListener('click', function(e) {
+      console.log(e.clientY, 'clientY');
+      console.log(e.clientX, 'clientX');
+
+      box.style.left = e.clientY + 'px';
+      box.style.top = e.clientX + 'px';
+
+    });
+
+
+  }

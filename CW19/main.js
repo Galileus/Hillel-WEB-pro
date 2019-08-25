115a1b801e5b2b2d975a5a08d1e9a091d09c2aa6
Ihor <galileyforever@gmail.com>
2019-08-25 14:59:26 +0300

    new changes cw19

--- CW19/main.js
+++ CW19/main.js
@@ -14,19 +14,28 @@ counter.innerHTML = localStorage.getItem('counter') || 0;
 for(var i = 0; i < boxes.length; i++) {
     boxes[i].dataset.id = i ;
 
+    
+
+
+
     boxes[i].querySelector('.counter').innerHTML  =  localStorage.getItem('counter' + boxes[i].dataset.id);
 
     boxes[i].addEventListener('click', function(event) {
         var counter = event.target.nextElementSibling;
         localStorage.setItem('counter' +  this.dataset.id, ++counter.innerHTML)
-    });
 
+    });
 
     res[i].addEventListener('click', function(){    	
 
-    	localStorage.setItem('counter' + 0, 0);
-    	console.log(i);
+    	localStorage.setItem('counter' + this.dataset.id, 0);
+
+    	console.log(boxes[0].dataset.id, 'this.dataset.id');
+
+
+    	
     });
+
 
 
 }
